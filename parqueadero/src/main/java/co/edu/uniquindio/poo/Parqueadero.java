d889d5a3a178f20e21c8e2349c24475b651dcb0d
jfsc27 <juanf.smithc@uqvirtual.edu.co>
2024-05-12 23:14:05 -0500

    Commit primeros metodos
    metodos para obtener el propietario y verificar puesto

--- parqueadero/src/main/java/co/edu/uniquindio/poo/Parqueadero.java
+++ parqueadero/src/main/java/co/edu/uniquindio/poo/Parqueadero.java
@@ -1,7 +1,6 @@
 package co.edu.uniquindio.poo;
 
 import java.util.Collection;
-import java.util.LinkedList;
 
 /**
 * Clase parqueadero.
@@ -15,48 +14,62 @@ public class Parqueadero {
      */
 
     private final String nombre;
-    private final Collection<Puesto> listaPuestos;
+    private final Puesto[][] listaPuestos;
+    private final int filas;
+    private final int columnas;
     private final Collection<Vehiculo> listaVehiculos;
     private final Collection<Registro> listaRegistroDiario;
     private final Collection<Registro> listaRegistroMensual;
 
-    /**
-     * Constructor.
-     *
-     */
-    public Parqueadero(String nombre) {
-
+    public Parqueadero(String nombre, int filas, int columnas, Collection<Vehiculo> listaVehiculos,
+            Collection<Registro> listaRegistroDiario, Collection<Registro> listaRegistroMensual) {
         this.nombre = nombre;
-        this.listaPuestos = new LinkedList<>();
-        this.listaVehiculos = new LinkedList<>();
-        this.listaRegistroDiario = new LinkedList<>();
-        this.listaRegistroMensual = new LinkedList<>();
-
-    }
-
-    /**
-     * Getters
-     *
-     */
-
-    public Collection<Puesto> getListaPuestos() {
-        return listaPuestos;
-    }
-
-    public Collection<Vehiculo> getListaVehiculos() {
-        return listaVehiculos;
+        this.columnas=columnas;
+        this.filas=filas;
+        this.listaPuestos = new Puesto[filas][columnas];
+        this.listaVehiculos = listaVehiculos;
+        this.listaRegistroDiario = listaRegistroDiario;
+        this.listaRegistroMensual = listaRegistroMensual;
     }
 
     public String getNombre() {
         return nombre;
     }
-
+    public Puesto[][] getListaPuestos() {
+        return listaPuestos;
+    }
+    public int getFilas() {
+        return filas;
+    }
+    public int getColumnas() {
+        return columnas;
+    }
+    public Collection<Vehiculo> getListaVehiculos() {
+        return listaVehiculos;
+    }
     public Collection<Registro> getListaRegistroDiario() {
         return listaRegistroDiario;
     }
-
     public Collection<Registro> getListaRegistroMensual() {
         return listaRegistroMensual;
     }
+
+    public boolean puestoEstaOcupado(int filas, int columnas) {
     
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                
+                if (listaPuestos[i][j].isEstado()==true) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private Propietario obtenerPropietarioVehiculo (Puesto puesto){
+        assert puesto.isEstado()==true;
+        return puesto.getVehiculo().getPropietario();
+
+    }
 }
