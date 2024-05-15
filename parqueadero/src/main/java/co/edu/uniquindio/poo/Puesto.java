c5007bba42f8e62f5a9184237eb59c136d2babbd
jfsc27 <juanf.smithc@uqvirtual.edu.co>
2024-05-15 17:36:59 -0500

    Commit

--- parqueadero/src/main/java/co/edu/uniquindio/poo/Puesto.java
+++ parqueadero/src/main/java/co/edu/uniquindio/poo/Puesto.java
@@ -11,30 +11,38 @@ public class Puesto {
      *
      */
 
-    private final PosicionPuesto puestos;
+    private final int [][] puestos;
+    private final int filas;
+    private final int columnas;
     private final Vehiculo vehiculo;
+    private boolean estado;
 
-    /**
-     * Constructor.
-     *
-     */
-
-    public Puesto(PosicionPuesto puestos, Vehiculo vehiculo) {
-        this.puestos = puestos;
+    public Puesto (int filas, int columnas, Vehiculo vehiculo, boolean estado) {
+        this.puestos = new int[filas][columnas];
+        this.filas = filas;
+        this.columnas = columnas;
         this.vehiculo = vehiculo;
+        this.estado = estado;
     }
 
-    /**
-     * Getters.
-     *
-     */
-    
-    public PosicionPuesto getPuestos() {
+    public int[][] getPuestos() {
         return puestos;
     }
-
+    public int getFilas() {
+        return filas;
+    }
+    public int getColumnas() {
+        return columnas;
+    }
     public Vehiculo getVehiculo() {
         return vehiculo;
     }
+    public boolean isEstado() {
+        return estado;
+    }
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
     
 }
